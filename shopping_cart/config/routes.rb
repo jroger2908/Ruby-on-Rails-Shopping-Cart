966292093144cc61f7a0342ae8bed96bab179e51
JR <jroger2908@gmail.com>
2015-04-27 17:32:13 -0400

    created routes file

--- shopping_cart/config/routes.rb
+++ shopping_cart/config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'order_items/create'
+  # get 'order_items/create'
 
-  get 'order_items/update'
+  # get 'order_items/update'
 
-  get 'order_items/destroy'
+  # get 'order_items/destroy'
 
-  get 'carts/show'
+  # get 'carts/show'
 
-  get 'products/index'
+  # get 'products/index'
+
+  resources :products, only: [:index]
+  resources :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
